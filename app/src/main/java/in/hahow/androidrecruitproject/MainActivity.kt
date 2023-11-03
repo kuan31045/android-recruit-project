19d61da9c4d96b9aa892c2e165489e29ea03a817
AKuan <kuan31045@gmail.com>
2023-11-03 15:56:38 +0800

    Add Compose theme

--- app/src/main/java/in/hahow/androidrecruitproject/MainActivity.kt
+++ app/src/main/java/in/hahow/androidrecruitproject/MainActivity.kt
@@ -3,14 +3,18 @@ package `in`.hahow.androidrecruitproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 import dagger.hilt.android.AndroidEntryPoint
+import `in`.hahow.androidrecruitproject.ui.theme.HahowTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            HahowTheme {
+                Text("hi")
+            }
         }
     }
 }
